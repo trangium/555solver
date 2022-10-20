ec661da0647a1ac83ff6dd291c254ceb3a7fd767
trangium <vincent.k.trang@gmail.com>
2022-10-20 08:49:04 -0400

    FInished implementing all moves of the 5x5x5 center cube

--- CenterCube.java
+++ CenterCube.java
@@ -5,11 +5,19 @@ public class CenterCube {
     private static final int[][][] moves = {
         {{0, 2, 4, 6}, {1, 3, 5, 7}}, // U
         {{24, 26, 28, 30}, {25, 27, 29, 31}}, // R
+        {{16, 18, 20, 22}, {17, 19, 21, 23}}, // F
+        {{40, 42, 44, 46}, {41, 43, 45, 47}}, // D
+        {{8, 10, 12, 14}, {9, 11, 13, 15}}, // L
+        {{32, 34, 36, 38}, {33, 35, 37, 39}}, // B
         {{0, 2, 4, 6}, {1, 3, 5, 7}, {8, 16, 24, 32}, {9, 17, 25, 33}, {10, 18, 26, 34}}, // Uw
-        {{24, 26, 28, 30}, {25, 27, 29, 31}, {32, 44, 20, 4}, {33, 45, 21, 5}, {34, 36, 22, 6}}, // Rw
+        {{24, 26, 28, 30}, {25, 27, 29, 31}, {32, 44, 20, 4}, {39, 43, 19, 3}, {38, 42, 18, 2}}, // Rw
+        {{16, 18, 20, 22}, {17, 19, 21, 23}, {4, 30, 40, 10}, {5, 31, 41, 11}, {6, 24, 42, 12}}, // Fw
+        {{40, 42, 44, 46}, {41, 43, 45, 47}, {36, 28, 20, 12}, {37, 29, 21, 13}, {38, 30, 22, 14}}, // Dw
+        {{8, 10, 12, 14}, {9, 11, 13, 15}, {0, 16, 40, 36}, {7, 23, 47, 35}, {6, 22, 46, 34}}, // Lw
+        {{32, 34, 36, 38}, {33, 35, 37, 39}, {0, 14, 44, 26}, {1, 15, 45, 27}, {2, 8, 46, 28}}, // Bw
     };
 
-    private static final List<String> moveStr = Arrays.asList("U", "R", "Uw", "Rw");
+    private static final List<String> moveStr = Arrays.asList("U", "R", "F", "D", "L", "B", "Uw", "Rw", "Fw", "Dw", "Lw", "Bw");
     public static final int numberOfMoves = moves.length;
 
     public CenterCube() {
@@ -31,7 +39,7 @@ public class CenterCube {
         return Arrays.hashCode(perm);
     }
 
-    public void printPzl() {
+    public void print() {
         String acc = "{";
         for (int i : perm) {
             acc = acc + i + ", ";
@@ -79,7 +87,7 @@ public class CenterCube {
     }
 
     public void executeStr(String sequence) {
-        for (String move : sequence.split("\n")) {
+        for (String move : sequence.split(" ")) {
             switch (move.charAt(move.length()-1)) {
                 case '\'': // counterclockwise
                     executeMove(moveStr.indexOf(move.substring(0, move.length()-1)), 3);
--- FiveByFiveSolver.java
+++ FiveByFiveSolver.java
@@ -1,5 +1,8 @@
 public class FiveByFiveSolver {
     public static void main(String[] args) {
-
+        CenterCube pzl = new CenterCube();
+        pzl.executeStr("R U Rw Uw Rw' Uw' Rw Uw Rw' Uw' Rw Uw Rw' Uw' Rw Uw Rw' Uw' Rw Uw Rw' Uw' Rw Uw Rw'");
+        pzl.executeMove(6, 3);
+        pzl.print();
     }
 }

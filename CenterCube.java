b471a89503e0f5df447c6884c6fc21b3596ae4fd
trangium <vincent.k.trang@gmail.com>
2022-10-30 08:55:43 -0400

    Made (reliable) HTR center solving
    Over 5 trials, the HTR centers take an average of 32.8 moves, and takes <2 seconds to solve.

--- CenterCube.java
+++ CenterCube.java
@@ -9,16 +9,17 @@ public class CenterCube {
         {{40, 42, 44, 46}, {41, 43, 45, 47}}, // D
         {{8, 10, 12, 14}, {9, 11, 13, 15}}, // L
         {{32, 34, 36, 38}, {33, 35, 37, 39}}, // B
-        {{0, 2, 4, 6}, {1, 3, 5, 7}, {8, 16, 24, 32}, {9, 17, 25, 33}, {10, 18, 26, 34}}, // Uw
+        {{0, 2, 4, 6}, {1, 3, 5, 7}, {8, 32, 24, 16}, {9, 33, 25, 17}, {10, 34, 26, 18}}, // Uw
         {{24, 26, 28, 30}, {25, 27, 29, 31}, {32, 44, 20, 4}, {39, 43, 19, 3}, {38, 42, 18, 2}}, // Rw
         {{16, 18, 20, 22}, {17, 19, 21, 23}, {4, 30, 40, 10}, {5, 31, 41, 11}, {6, 24, 42, 12}}, // Fw
-        {{40, 42, 44, 46}, {41, 43, 45, 47}, {36, 28, 20, 12}, {37, 29, 21, 13}, {38, 30, 22, 14}}, // Dw
+        {{40, 42, 44, 46}, {41, 43, 45, 47}, {36, 12, 20, 28}, {37, 13, 21, 29}, {38, 14, 22, 30}}, // Dw
         {{8, 10, 12, 14}, {9, 11, 13, 15}, {0, 16, 40, 36}, {7, 23, 47, 35}, {6, 22, 46, 34}}, // Lw
         {{32, 34, 36, 38}, {33, 35, 37, 39}, {0, 14, 44, 26}, {1, 15, 45, 27}, {2, 8, 46, 28}}, // Bw
     };
     public static final List<String> moveStr = Arrays.asList("U", "R", "F", "D", "L", "B", "Uw", "Rw", "Fw", "Dw", "Lw", "Bw");
+    public static final List<String> moveAmts = Arrays.asList("", "2", "'");
     public static final int numberOfMoves = moves.length;
-    public static final byte[] solved = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5};
+    public static final byte[] solved = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0};
     public static final int numberOfPieces = solved.length;
 
     public CenterCube() {
@@ -63,14 +64,14 @@ public class CenterCube {
         assert positions.length == 4;
         byte temp;
         switch (amount) {
-            case 1: // clockwise
+            case 2: // clockwise
                 temp = perm[positions[0]];
                 perm[positions[0]] = perm[positions[1]];
                 perm[positions[1]] = perm[positions[2]];
                 perm[positions[2]] = perm[positions[3]];
                 perm[positions[3]] = temp;
                 return;
-            case 2: // double
+            case 1: // double
                 temp = perm[positions[0]];
                 perm[positions[0]] = perm[positions[2]];
                 perm[positions[2]] = temp;
@@ -78,7 +79,7 @@ public class CenterCube {
                 perm[positions[1]] = perm[positions[3]];
                 perm[positions[3]] = temp;
                 return;
-            case 3: // inverse
+            case 0: // inverse
                 temp = perm[positions[0]];
                 perm[positions[0]] = perm[positions[3]];
                 perm[positions[3]] = perm[positions[2]];
@@ -98,15 +99,24 @@ public class CenterCube {
         for (String move : sequence.split(" ")) {
             switch (move.charAt(move.length()-1)) {
                 case '\'': // counterclockwise
-                    executeMove(moveStr.indexOf(move.substring(0, move.length()-1)), 3);
-                    break;
-                case '2':
                     executeMove(moveStr.indexOf(move.substring(0, move.length()-1)), 2);
                     break;
+                case '2':
+                    executeMove(moveStr.indexOf(move.substring(0, move.length()-1)), 1);
+                    break;
                 default:
-                    executeMove(moveStr.indexOf(move), 1);
+                    executeMove(moveStr.indexOf(move), 0);
                     break;
             }
         }
     }
+
+    // public String moveListToStr(int[] sequence) {
+    //     String acc = "";
+    //     for (int move : sequence) {
+    //         acc += moveStr.get(move/3);
+    //         if (acc%3)
+    //     }
+    //     return acc.substring(0, acc.length()-1);
+    // }
 }
--- FiveByFiveSolver.java
+++ FiveByFiveSolver.java
@@ -1,13 +1,135 @@
 import java.util.*;
 
 public class FiveByFiveSolver {
+    static final int UBL = 0;
+    static final int UB = 1;
+    static final int UBR = 2;
+    static final int UR = 3;
+    static final int UFR = 4;
+    static final int UF = 5;
+    static final int UFL = 6;
+    static final int UL = 7;
 
-    private static int h(CenterCube pzl) { // 1, 13, 19
+    static final int LUB = 8;
+    static final int LU = 9;
+    static final int LUF = 10;
+    static final int LF = 11;
+    static final int LDF = 12;
+    static final int LD = 13;
+    static final int LDB = 14;
+    static final int LB = 15;
+
+    static final int FUL = 16;
+    static final int FU = 17;
+    static final int FUR = 18;
+    static final int FR = 19;
+    static final int FDR = 20;
+    static final int FD = 21;
+    static final int FDL = 22;
+    static final int FL = 23;
+
+    static final int RUF = 24;
+    static final int RU = 25;
+    static final int RUB = 26;
+    static final int RB = 27;
+    static final int RDB = 28;
+    static final int RD = 29;
+    static final int RDF = 30;
+    static final int RF = 31;
+
+    static final int BUR = 32;
+    static final int BU = 33;
+    static final int BUL = 34;
+    static final int BL = 35;
+    static final int BDL = 36;
+    static final int BD = 37;
+    static final int BDR = 38;
+    static final int BR = 39;
+
+    static final int DFL = 40;
+    static final int DF = 41;
+    static final int DFR = 42;
+    static final int DR = 43;
+    static final int DBR = 44;
+    static final int DB = 45;
+    static final int DBL = 46;
+    static final int DL = 47;
+
+    private static boolean pcsSolvedHTR(CenterCube pzl, int[] positions) { // reduce to HTR
+        for (int pos : positions) {
+            if (pzl.getPerm()[pos] != CenterCube.solved[pos]) return false;
+        }
+        return true;
+    }
+
+    private static boolean anySetSolvedHTR(CenterCube pzl, int[][] positionsSet) {
+        for (int[] positions : positionsSet) {
+            if (pcsSolvedHTR(pzl, positions)) return true;
+        }
+        return false;
+    }
+
+    private static int countSolvedHTR(CenterCube pzl, int[] positions, int pseudo) {
         int acc = 0;
-        if (pzl.getPerm()[1] != 0) acc++;
-        if (pzl.getPerm()[13] != 1) acc++;
-        if (pzl.getPerm()[19] != 2) acc++;
+        for (int pos : positions) {
+            if (pzl.getPerm()[pos] == pseudo) acc += 1;
+        }
         return acc;
+    }
+
+    private static boolean pcsSolvedWeird(CenterCube pzl, int[] positions, int pseudo) { // reduce to HTR
+        for (int pos : positions) {
+            if (pzl.getPerm()[pos] != pseudo) return false;
+        }
+        return true;
+    }
+
+    private static boolean anySetSolvedWeird(CenterCube pzl, int[][] positionsSet, int pseudo) {
+        for (int[] positions : positionsSet) {
+            if (pcsSolvedWeird(pzl, positions, pseudo)) return true;
+        }
+        return false;
+    }
+
+    private static int h_helper(CenterCube pzl) {
+        if (!anySetSolvedHTR(pzl, new int[][] {{UB}, {UR}, {UF}, {UL}})) return 4000;
+        if (!anySetSolvedHTR(pzl, new int[][] {{UB, UBR, UR}, {UR, UFR, UF}, {UF, UFL, UL}, {UL, UBL, UB}})) return 3900;
+        if (!anySetSolvedHTR(pzl, new int[][] {{UB, UBR, UR, UFR, UF}, {UR, UFR, UF, UFL, UL}, {UF, UFL, UL, UBL, UB}, {UL, UBL, UB, UBR, UR}})) return 3800;
+        if (!anySetSolvedHTR(pzl, new int[][] {{DB}, {DR}, {DF}, {DL}})) return 3700;
+        if (!anySetSolvedHTR(pzl, new int[][] {{DB, DBL, DL}, {DF, DFL, DL}, {DB, DBR, DR}, {DF, DFR, DR}})) return 3600;
+        if (!anySetSolvedHTR(pzl, new int[][] {{DB, DBR, DR, DFR, DF}, {DR, DFR, DF, DFL, DL}, {DF, DFL, DL, DBL, DB}, {DL, DBL, DB, DBR, DR}})) return 3500;
+        if (!anySetSolvedHTR(pzl, new int[][] {{0, 1, 2, 3, 4, 5, 6, 7, 40, 41, 42, 43, 44, 45, 46, 47}})) {
+            if (!anySetSolvedWeird(pzl, new int[][] {{FUR, FR, FDR, BUR, BR, BDR}, {FUL, FL, FDL, BUL, BL, BDL}}, 0)) {
+                int topHTR = countSolvedHTR(pzl, new int[] {0, 1, 2, 3, 4, 5, 6, 7}, 0);
+                int bottomHTR = countSolvedHTR(pzl, new int[] {40, 41, 42, 43, 44, 45, 46, 47}, 0);
+                if (topHTR != 8 && topHTR != 5) return 3400;
+                if (bottomHTR != 8 && bottomHTR != 5) return 3300;
+                if (topHTR != 8 && bottomHTR != 8) return 3200;
+                if (!anySetSolvedWeird(pzl, new int[][] {{FUL, FU, FUR}, {FUR, FR, FDR}, {FDR, FD, FDL}, {FDL, FL, FUL}, {BUL, BU, BUR}, {BUR, BR, BDR}, {BDR, BD, BDL}, {BDL, BL, BUL}}, 0)) {
+                    if (!anySetSolvedWeird(pzl, new int[][] {{FUL, FU}, {FU, FUR}, {FUR, FR}, {FR, FDR}, {FDR, FD}, {FD, FDL}, {FDL, FL}, {FL, FUL}, {BUL, BU}, {BU, BUR}, {BUR, BR}, {BR, BDR}, {BDR, BD}, {BD, BDL}, {BDL, BL}, {BL, BUL}}, 0)) return 3100;
+                    return 3000;
+                }
+                return 2900;
+            }
+            return 2800;
+        }
+        if (!anySetSolvedHTR(pzl, new int[][] {{LU}, {LF}, {LD}, {LB}})) return 2700;
+        if (!anySetSolvedHTR(pzl, new int[][] {{LU, LUF, LF}, {LF, LDF, LD}, {LD, LDB, LB}, {LB, LUB, LU}})) return 2600;
+        if (!anySetSolvedHTR(pzl, new int[][] {{LU, LUF, LF, LDF, LD}, {LF, LDF, LD, LDB, LB}, {LD, LDB, LB, LUB, LU}, {LB, LUB, LU, LUF, LF}})) return 2500;
+        if (!anySetSolvedHTR(pzl, new int[][] {{RU}, {RF}, {RD}, {RB}})) return 2400;
+        if (!anySetSolvedHTR(pzl, new int[][] {{RU, RUF, RF}, {RF, RDF, RD}, {RD, RDB, RB}, {RB, RUB, RU}})) return 2300;
+        if (!anySetSolvedHTR(pzl, new int[][] {{RU, RUF, RF, RDF, RD}, {RF, RDF, RD, RDB, RB}, {RD, RDB, RB, RUB, RU}, {RB, RUB, RU, RUF, RF}})) return 2200;
+        if (!anySetSolvedHTR(pzl, new int[][] {{8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30, 31}})) {
+            if (!anySetSolvedWeird(pzl, new int[][] {{FUL, FU, FUR}, {FUR, FR, FDR}, {FDR, FD, FDL}, {FDL, FL, FUL}}, 1)) return 2100;
+            if (!anySetSolvedWeird(pzl, new int[][] {{BUL, BU, BUR}, {BUR, BR, BDR}, {BDR, BD, BDL}, {BDL, BL, BUL}}, 1)) return 2000;
+            return 1900;
+        }
+        if (!isSolved(pzl)) return 1800;
+        return 0;
+    }
+
+    private static int h(CenterCube pzl) {
+        return h_helper(pzl) / 50;
     }
 
     private static boolean isSolved(CenterCube pzl) {
@@ -17,8 +139,8 @@ public class FiveByFiveSolver {
         return true;
     }
 
-    private static int solve(CenterCube rootPzl) {
-        final int MAXDEPTH = 20;
+    private static String solveCenters(CenterCube rootPzl) {
+        final int MAXDEPTH = 4020;
         List<Stack<CenterCube>> openSetPzls = new ArrayList<Stack<CenterCube>>();
         for (int i=0; i<MAXDEPTH; i++) {
             openSetPzls.add(new Stack<CenterCube>());
@@ -40,22 +162,42 @@ public class FiveByFiveSolver {
             }
             CenterCube pzl = openSetPzls.get(min_f).pop();
             Integer moveIndex = openSetMoves.get(min_f).pop();
+            int popped_f = min_f;
 
             if (closedSet.containsKey(pzl)) continue;
             closedSet.put(pzl, moveIndex);
 
-            if (isSolved(pzl)) return min_f;
+            if (isSolved(pzl)) return getSolution(closedSet, rootPzl, pzl);
 
             for (int nextMoveIndex=0; nextMoveIndex < CenterCube.numberOfMoves; nextMoveIndex++) {
-                for (int rot=1; rot<=3; rot++) {
+                for (int rot=0; rot<=2; rot++) {
                     CenterCube nbr = new CenterCube(pzl);
                     nbr.executeMove(nextMoveIndex, rot);
-                    int new_f = h(nbr)+min_f-h(pzl)+1;
-                    openSetPzls.get(new_f).push(nbr);
-                    openSetMoves.get(new_f).push(nextMoveIndex*3+rot); // number from 1 to 36 representing both the type and direction of the move
+                    int new_f = h(nbr)+popped_f-h(pzl)+1;
+                    if (new_f < min_f) {
+                        min_f = new_f;
+                        // System.out.println(min_f);
+                        // System.out.println(getSolution(closedSet, rootPzl, new CenterCube(pzl))+CenterCube.moveStr.get(nextMoveIndex)+CenterCube.moveAmts.get(rot));
+                    }
+                    if (!nbr.equals(pzl)) {
+                        openSetPzls.get(new_f).push(nbr);
+                        openSetMoves.get(new_f).push(nextMoveIndex*3+rot); // number from 1 to 36 representing both the type and direction of the move
+                    }
                 }
             }
         }
+    }
+
+    private static String getSolution(HashMap<CenterCube, Integer> closedSet, CenterCube rootPzl, CenterCube pzl) {
+        String solution = "";
+        while (!pzl.equals(rootPzl)) {
+            int moveData = closedSet.get(pzl);
+            int rot = moveData%3;
+            int ind = moveData/3;
+            solution = CenterCube.moveStr.get(ind) + CenterCube.moveAmts.get(rot) + " " + solution;
+            pzl.executeStr(CenterCube.moveStr.get(ind) + CenterCube.moveAmts.get(2-rot));
+        }
+        return solution;
     }
 
     /*
@@ -81,7 +223,10 @@ public class FiveByFiveSolver {
 
     public static void main(String[] args) {
         CenterCube pzl = new CenterCube();
-        pzl.executeStr("Rw F2 Rw Uw L");
-        System.out.println(solve(pzl));
+        String scramble = "D2 Uw F R Uw U Rw' Dw' F D F' Rw2 Bw' Rw U Bw2 Dw F Rw Fw F2 R' Bw' L D R Rw' L Fw' B2 Dw Uw Rw' B' F2 U2 Bw2 Lw2 B Dw' Bw' B' Fw Dw' U2 Fw F Uw L' B2 D' B Bw D Dw R2 Bw2 F2 L2 Rw";
+
+        pzl.executeStr(scramble);
+        System.out.println(scramble+"\n");
+        System.out.println(solveCenters(pzl));
     }
 }
